3f18d1cee44fd0cb1cedbaced68a76951e563c06
volumit <vo_lumit@gmx.de>
2022-01-28 18:38:14 +0100

    reconfig
    change config.py in a way that all directories, tool options, names,
    etc. are handled in this file

--- random-insn-test/Config.py
+++ random-insn-test/Config.py
@@ -3,19 +3,27 @@
 ##  Config
 ##
 ###########################
-prefix = "../tricore-binutils/install/bin/"
+prefix = "/home/dummy/tricore_494_linux/bin/"
 
-tas     = prefix + "tricore-as"
-tld     = prefix + "tricore-ld"
+tas     = prefix + "tricore-elf-as"
+tas_flags ="-mtc162 -o0"
 
-tsim = "tsim"
-qemu = "../../qemu/build/tricore-softmmu/qemu-system-tricore"
-version ="13"
+tld     = prefix + "tricore-elf-ld"
+tld_flags = "--mcpu=tc162 -T target.ld "
 
-ldscript = "target.ld"
+tobjdump     = prefix + "tricore-elf-objdump"
+tobjdump_flags = "-D "
+
+tsim = "/home/dummy/BUILD/build_linux_gcc494/testsuite/gcc/tsim16p_e"
+#tsim_flags = "-DConfig ../DCONFIG -MConfig ../MCONFIG -e -disable-watchdog -x 100000 -o "
+tsim_flags = "-tc162p -DConfig ../DCONFIG -MConfig ../MCONFIG -e -disable-watchdog -x 100000 -o "
+
+qemu = "/home/dummy/qemu_tricore_6250/bin/qemu-system-tricore"
+#qemu_flags = " -M tricore_tsim162 -display none -singlestep -D qemu.result -d nochain,exec,cpu "
+qemu_flags = " -M tricore_tsim162 -nographic -singlestep -D qemu.result -d nochain,exec,cpu "
 
 baseJumpReg = 14
 baseMemReg = 15
 
-maxInsnPerTest = 2000
+maxInsnPerTest = 10000
 
